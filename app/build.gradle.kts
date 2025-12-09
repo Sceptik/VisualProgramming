dff5d4b4a4291f8eb6c99aedc179523c6e28dac9
sceptik <iluha154russick@gmail.com>
2025-12-09 17:17:46 +0700

    2 semestr

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.location)
+    implementation ("org.zeromq:jeromq:0.5.0") // -->> JeroMQ
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/myapp/Location.kt
+++ app/src/main/java/com/example/myapp/Location.kt
@@ -19,23 +19,49 @@ import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 import android.widget.TextView
 import android.location.Location
+import android.os.CountDownTimer
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.io.File
 import java.io.FileWriter
 import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.annotations.Async.Execute
+import org.zeromq.SocketType
+import org.zeromq.ZContext
+import org.zeromq.ZMQ
+import android.content.Context
+import android.net.NetworkInfo
+import android.telephony.TelephonyManager
 
 class Location : AppCompatActivity() {
     private val LOCATION_PERMISSION_REQUEST_CODE = 1001
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-
+    var previousData = ""
     private lateinit var Latitude: TextView
     private lateinit var Longitude: TextView
     private lateinit var Altitude: TextView
     private lateinit var Current_time: TextView
 
+    private lateinit var _cellID: TextView
+    private lateinit var _pci: TextView
+    private lateinit var _rsrp: TextView
+    private lateinit var _bandwith: TextView
+
+
     private val gson = Gson()
+    private var timer: CountDownTimer? = null
+    val TAG = "TelephonyActivity"
+
+    var jsonString: String? = null
+    private val log_tag: String = "MY_LOG_TAG"
+    private var clientRunning = false
+    private var getting_data = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,14 +77,67 @@ class Location : AppCompatActivity() {
 
         UI()
         checkLocationPermission()
+        startClientInBackground()
+    }
+
+    private fun start_getting_data(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback(){
+                override fun onLocationResult(p0: LocationResult) {
+                    p0.lastLocation?.let{SaveDataAndUpdateUI(it, getNetworkInfo())}
+                }
+            },
+            Looper.getMainLooper()
+            )
+    }
+
+    private fun getNetworkInfo(): NetworkData?{
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return null
+        }
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val cellInfoList = telephonyManager.allCellInfo
+
+        for(cell in cellInfoList){
+            if(cell is android.telephony.CellInfoLte && cell.isRegistered){
+                return NetworkData(
+                    cellID = cell.cellIdentity.ci,
+                    pci = cell.cellIdentity.pci,
+                    rsrp = cell.cellSignalStrength.rsrp,
+                    band = cell.cellIdentity.bandwidth
+//                    rsrq = cell.cellSignalStrength.rsrq
+                )
+            }
+        }
+        return null
     }
     private fun checkLocationPermission() {
         when {
             ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
             ) == PackageManager.PERMISSION_GRANTED -> {
-                getLastLocation()
+                start_getting_data()
             }
             ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
@@ -78,9 +157,10 @@ class Location : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             LOCATION_PERMISSION_REQUEST_CODE -> {
-                if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    getLastLocation()
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    start_getting_data()
                 }
                 return
             }
@@ -92,7 +172,8 @@ class Location : AppCompatActivity() {
             this,
             arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE
             ),
             LOCATION_PERMISSION_REQUEST_CODE
         )
@@ -104,13 +185,20 @@ class Location : AppCompatActivity() {
         Longitude = findViewById(R.id.Longitude)
         Altitude = findViewById(R.id.Altitude)
         Current_time = findViewById(R.id.Current_time)
+        _cellID = findViewById(R.id.CellID)
+        _pci = findViewById(R.id.pci)
+        _rsrp = findViewById(R.id.rsrp)
+        _bandwith = findViewById(R.id.bandwith)
     }
 
-    private fun SaveDataAndUpdateUI(location: Location){
+    private fun SaveDataAndUpdateUI(location: Location, networkData: NetworkData?){
         Latitude.text = "Latitude: ${location.latitude}"
         Longitude.text = "Longitude: ${location.longitude}"
         Altitude.text = "Altitude: ${location.altitude} meters"
-
+        _cellID.text = "Cell id : ${networkData?.cellID}"
+        _pci.text = "pci : ${networkData?.pci}"
+        _rsrp.text = "rsrp : ${networkData?.rsrp}"
+        _bandwith.text = "bandwith : ${networkData?.band}"
 
 
         val sdf = SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.getDefault())
@@ -124,42 +212,93 @@ class Location : AppCompatActivity() {
             output_altitude = location.altitude,
             output_Current_time = location.time
         )
-        saveLocationDataToJSON(locationData)
+        saveLocationDataToJSON(locationData, networkData)
     }
 
-    private fun saveLocationDataToJSON(locationData: LocationData) {
-        val file = File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "location_data.json")
+    private fun startClientInBackground() {
+        clientRunning = true;
+        lifecycleScope.launch(Dispatchers.IO) {
+            while(clientRunning)
+                try{
+                    startClient()
+                    delay(5000)
+                } catch(e: Exception) {
+                    Log.e(log_tag, "client error")
+                    delay(5000)
+                }
+        }
+    }
+    fun startClient() {
+        val context = ZMQ.context(1)
+        val socket = context.socket(ZMQ.REQ)
+
+        try{
+            socket.connect("tcp://192.168.133.53:2222")
+            socket.setReceiveTimeOut(10000)
+
+            val currentData  = readJsonFile()
+
+            if(currentData.isNotEmpty()){
+                socket.send(currentData.toByteArray(ZMQ.CHARSET))
+
+                val reply = socket.recvStr()
+                if(reply == null){
+                    Log.d(log_tag, "client: server didn't response")
+                } else {
+                    Log.d(log_tag, "client: received $reply")
+                }
+            }else {
+                Thread.sleep(5000)
+            }
+        } catch(e: Exception){
+            Log.e(log_tag, "client error: ${e.message}")
+        } finally {
+            socket.close()
+            context.close()
+        }
+    }
+
+    private fun readJsonFile() : String {
+        val documentsDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val file = File(documentsDir, "all_data.json")
+
+        if (file.exists()) {
+            return file.readText()
+        } else {
+            return "Nothing to transmit"
+        }
+    }
+
+
+    private fun saveLocationDataToJSON(locationData: LocationData, networkData: NetworkData?) {
+        val documentsDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val file = File(documentsDir, "all_data.json")
 
         if (!file.exists()) {
             file.createNewFile()
         }
-
+        val combinedData = CombinedData(locationData, networkData)
         FileWriter(file, true).use { writer ->
-            writer.append("${gson.toJson(locationData)}\n")
+            writer.append("${gson.toJson(combinedData)}\n")
         }
     }
 
-    private fun getLastLocation(){
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                location?.let {
-                    SaveDataAndUpdateUI(it)
-                }
-            }
-    }
 }
-
+data class CombinedData(
+    val location : LocationData,
+    val network : NetworkData?
+)
 data class LocationData(
     val output_latitude: Double,
     val output_longitude: Double,
     val output_altitude: Double,
     val output_Current_time: Long
+)
+
+data class NetworkData(
+    val cellID : Int,
+    val pci : Int,
+    val rsrp : Int,
+    val band : Int,
+//    val rsrq : Int
 )
